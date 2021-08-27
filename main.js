2cc053cf0c9eba502d6822877b1d73ca6a47de3a
Fredy Arias <fredy.arias@grupor5.com>
2021-08-26 19:48:57 -0500

    Se crean y se ubican todas las funciones de las opciones del menú

--- main.js
+++ main.js
@@ -1,35 +1,59 @@
-function menu (opcion){
+let opcion = 5;
+
+function showMenu() {
     console.clear();
     console.log('Hola, bienvenido al ToDo List, Ingrese:\n')
     console.log('1. Para crear una nueva tarea');
-    console.log('2. Para buscar una tarea');
+    console.log('2. Para listar las tareas');
     console.log('3. Para modificar una tarea');
     console.log('4. Para eliminar una tarea');
     console.log('5. Para salir\n');
+}
+
 do {
+    showMenu()
     switch (opcion) {
         case 1: 
-            console.log('Creando nueva tarea')
+            createTask()
             break;
         case 2: 
-            console.log('Buscando tarea')
+            listTask()
             break
         case 3: 
-            console.log('Modificando tarea')
+            updateTask()
             break
         case 4: 
-            console.log('Eliminando tarea')
+            delateTask()
             break
         case 5:
-            console.log('Saliendo...'); 
+             exitMenu()
             break   
         default:
             console.log('No es una opción valida');
             break;
         }
     } while (opcion != 5)
+
+function createTask() {
+    console.log('Creando nueva tarea')
 }
 
-menu(5);
+function listTask() {
+    console.log('Listando tareas')
+}
+
+function updateTask() {
+    console.log('Actualizando tarea')
+}
+
+function delateTask() {
+    console.log('Eliminando tarea')
+}
+
+function exitMenu(){
+    console.log('Saliendo...');
+}
+
+showMenu(opcion);
 
     
