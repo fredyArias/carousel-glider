d146d55b484f2f54a1e151f65210a25b578d8aca
Fredy Arias <fredy.arias@grupor5.com>
2021-09-08 18:52:37 -0500

    Primera parte del carousel, falta animación, conteo, probar otros diseños

---
+++ js/slider.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel-list'), {
+    sslidesToShow: 5,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.carousel-indicators',
+    arrows: {
+        prev: '.carousel-back',
+        next: '.carousel-next'
+        }    
+    
+    });
+});
+
+window.addEventListener('load', function(){
+new Glider(document.querySelector('.carousel-list-2'), {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.carousel-indicators-2',
+    arrows: {
+        prev: '.carousel-back',
+        next: '.carousel-next'
+        }    
+    
+    });
+});
+
+
+
+
+
+
+
+
+
+
