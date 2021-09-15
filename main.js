079587e5a167402fec54e680c04d7db4149bd331
Fredy Arias <fredy.arias@grupor5.com>
2021-09-15 11:12:21 -0500

    Solo borrado de espacios innecesarios

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-function showMenu() {
-    do {
-        console.clear();
-        console.log(`Hola, bienvenido al ToDo List, Ingrese:
-        1. Para crear una nueva tarea
-        2. Para listar las tareas
-        3. Para modificar una tarea
-        4. Para eliminar una tarea
-        5. Para salir
-        `);
+function showMenu(additaionalMessage =  '') {
 
+        printMenuPrincipal(additaionalMessage)
         let stdin = process.openStdin()
         stdin.addListener('data', function (d) {
-        const opcion = d.toString().trim()
-        switch (opcion) {
+            console.log(d)
+        const currentOption = d.toString().trim()
+        switch (currentOption) {
             case '1': 
                 createTask()
                 break;
@@ -33,18 +26,68 @@ function showMenu() {
                 break;
             }
         })
-    } while(opcion =! 5);
-    
 }
 
-showMenu()
+const toDoList = []
+
+function printMenuPrincipal(additaionalMessage){
+    console.clear();
+    console.log(`${additaionalMessage}
+    Hola, bienvenido al ToDo List, Ingrese:
+    1. Para crear una nueva tarea
+    2. Para listar las tareas
+    3. Para modificar una tarea
+    4. Para eliminar una tarea
+    5. Para salir
+    `);
+}
 
 function createTask() {
+    console.clear();
+    console.log(`esscriba la tarea 
+        si quieres cerra el programa selecciona 1
+    `);
+    let stdin = process.openStdin()
+
+    stdin.addListener('data', function (d) {
+        const currentOption = d.toString().trim()
+        switch (currentOption) {
+            case '1':
+                process.exit() 
+            break;
+            default:
+                toDoList.push(currentOption)
+                showMenu("mensaje guardado")
+                console.log(stdin,'----')
+                stdin.exit()
+            break;
+            }
+        })
     console.log('Creando nueva tarea')
 }
 
 function listTask() {
-    console.log('Listando tareas')
+
+    console.clear()
+    console.log('Listando tareas que son')
+    for( let index in toDoList){
+        console.log(`tarea ${index + 1} ${toDoList[index]}`)
+    }
+    console.log(` selecccion 1 para terminar o cualquoer texto para ir al menu principal`);
+    let stdin = process.openStdin()
+    stdin.addListener('data', function (d) {
+        const currentOption = d.toString().trim()
+        switch (currentOption) {
+            case '1':
+                process.exit() 
+            break;
+            default:
+                showMenu()
+            break;
+            }
+        })
+
+
 }
 
 function updateTask() {
@@ -58,6 +101,8 @@ function delateTask() {
 function exitMenu(){
     console.log('Saliendo...');
 }
+
+showMenu()
 
 
 
